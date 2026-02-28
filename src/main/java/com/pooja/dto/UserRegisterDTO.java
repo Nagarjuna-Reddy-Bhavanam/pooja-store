5a600a3d2107de58ad49d43c6d782ad17486970f
Nagarjuna-Reddy-Bhavanam <134576802+Nagarjuna-Reddy-Bhavanam@users.noreply.github.com>
2026-02-28 18:08:21 +0530

    Add UserRegisterDTO with validation constraints
    Created UserRegisterDTO class with validation annotations for user registration fields.

---
+++ src/main/java/com/pooja/dto/UserRegisterDTO.java
@@ -0,0 +1,45 @@
+package com.pooja.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserRegisterDTO {
+    @NotBlank(message = "Email is required")
+    @Email(message = "Email should be valid")
+    private String email;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 6, message = "Password should be at least 6 characters")
+    private String password;
+
+    @NotBlank(message = "First name is required")
+    private String firstName;
+
+    @NotBlank(message = "Last name is required")
+    private String lastName;
+
+    @NotBlank(message = "Phone is required")
+    private String phone;
+
+    @NotBlank(message = "Address is required")
+    private String address;
+
+    @NotBlank(message = "City is required")
+    private String city;
+
+    @NotBlank(message = "State is required")
+    private String state;
+
+    @NotBlank(message = "Zip code is required")
+    private String zipCode;
+
+    @NotBlank(message = "Country is required")
+    private String country;
+}
