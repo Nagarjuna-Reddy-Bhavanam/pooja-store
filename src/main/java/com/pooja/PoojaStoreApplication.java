f797d8dc0bbbceac298c672b5c5015dda5db1319
Nagarjuna-Reddy-Bhavanam <134576802+Nagarjuna-Reddy-Bhavanam@users.noreply.github.com>
2026-02-28 17:56:45 +0530

    Initialize Spring Boot application structure

---
+++ src/main/java/com/pooja/PoojaStoreApplication.java
@@ -0,0 +1,12 @@
+package com.pooja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoojaStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PoojaStoreApplication.class, args);
+    }
+}
