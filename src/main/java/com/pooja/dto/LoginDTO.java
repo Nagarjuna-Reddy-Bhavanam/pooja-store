3f0555b10820e73c6aaf9f5a3729f9c5d07e2e98
Nagarjuna-Reddy-Bhavanam <134576802+Nagarjuna-Reddy-Bhavanam@users.noreply.github.com>
2026-02-28 18:18:03 +0530

    LoginDTO

--- src/main/java/com/pooja/dto/LoginDTO.java
+++ src/main/java/com/pooja/dto/LoginDTO.java
@@ -1,23 +1,19 @@
 package com.pooja.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class LoginDTO {
+    @NotBlank(message = "Email is required")
+    @Email(message = "Email should be valid")
     private String email;
+
+    @NotBlank(message = "Password is required")
     private String password;
-
-    // Getters and Setters
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
